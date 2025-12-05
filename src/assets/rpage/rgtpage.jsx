d07b826aa19262dfb91c7589ec476a9c582c37cc
dongyu <henido157@gmail.com>
2025-12-05 11:37:45 +0900

    메인 페이지 등록 페이지

--- src/assets/rpage/rgtpage.jsx
+++ src/assets/rpage/rgtpage.jsx
@@ -1,10 +1,40 @@
 import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import "./rgtpage.css";
 
 export default function RgtPage() {
 
+    const navigate = useNavigate(); //취소시 메인페이지
+
     const [title, setTitle] = useState("");
     const [content, setContent] = useState("");
+
+    const submitbook = () => {
+        // 제목 확인
+        if(title.trim() === ""){
+            alert("제목 입력");
+            return;
+        }
+        //책 정보 입력
+        const newbook_load = {
+            id: Date.now(),
+            title: title,
+            content: content,
+            img: null
+        };
+        // 작품 목록 가져오기
+        const saved = JSON.parse(localStorage.getItem("items")) || [];
+
+        //데이터 저장
+        localStorage.setItem("items", JSON.stringify([...saved, newbook_load]));
+
+        alert("등록 완료!");
+
+        // 메인페이지로 이동
+        navigate("/");
+
+
+    }
 
     return (
         <div className="register-container">
@@ -16,7 +46,6 @@ export default function RgtPage() {
 
             {/* 본문 */}
             <div className="register-box">
-
                 {/* 이미지 업로드 영역 */}
                 <div className="image-area">
                     작품이미지
@@ -24,7 +53,6 @@ export default function RgtPage() {
 
                 {/* 입력 영역 */}
                 <div className="input-area">
-
                     <label className="label">도서 제목</label>
                     <input
                         type="text"
@@ -44,8 +72,16 @@ export default function RgtPage() {
 
             {/* 하단 버튼 */}
             <div className="btn-area">
-                <button className="cancel-btn">취소</button>
-                <button className="submit-btn">등록</button>
+                <button className="cancel-btn"
+                        onClick={() => navigate("/")}>
+                    취소
+                </button>
+
+
+                <button className="submit-btn"
+                        onClick={submitbook}>
+                    등록
+                </button>
             </div>
         </div>
     );
